9970f617c8399936e4fa7dd009b1291247099a45
TacticalPotato03 <travisdc@pm.me>
2026-03-04 08:45:02 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   integrations: [mdx(), sitemap()],
   site: 'https://travis-campbell.com',
+  trailingSlash: 'always',
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
